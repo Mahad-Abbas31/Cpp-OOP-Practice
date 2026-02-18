1ecc3773cc59b3c416d8a38206803ae890af9186
Mahad Abbas <mahadabbas31@gmail.com>
2026-02-18 17:03:37 +0500

    Copy Constructor Concept Polished

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -11,7 +11,7 @@ using namespace std;
  -> WHY USED?
      - C++ provides a default copy constructor, but it performs a shallow copy.
      - If your class uses pointers or dynamic memory, you need a user-defined copy constructor 
-     to perform a deep copy.
+       to perform a deep copy.
     
 */
 
@@ -23,16 +23,6 @@ class Hero{
     int stars;
     
 public:
-    //___ Copy Constructor Method/Function
-    Hero(Hero &orgObj){               //--> Pass by Reference
-        cout << "I am Custom Copy Constructor\n";
-        this->name = orgObj.name;
-        this->power = orgObj.power;
-        this->region = orgObj.region;
-        this->level = orgObj.level;
-        this->stars = orgObj.stars;
-    }
-
     //__ Using "this" to resolve naming conflict
     Hero(string n, string p, string r, int l, int s){
         name = n;
@@ -44,17 +34,60 @@ public:
 
     //__ Display function
     void display(){
-        cout << name << endl;
+        cout << endl << name << endl;
         cout << power << endl;
         cout << region << endl;
         cout << level << endl;
         cout << stars << endl;
     }
-
-
 };
 
+class Student{
+    string name;
+    int age;
+    int *ptr;
+    
+public:
+    //___ Copy Constructor Method/Function
+    Student(const Student &orgObj){               //--> Pass by Reference
+        cout << "\nI am Custom Copy Constructor\n";
+        name = orgObj.name;
+        age = orgObj.age;
+        ptr = new int;
+        *ptr = *(orgObj.ptr);
+        *ptr = 100;
+        cout << "I am Deep Copy\n";
+    }
 
+    // ___ Shallow Copy 
+    // Student(Student &orgObj){               //--> Pass by Reference
+    //     cout << "I am Custom Copy Constructor\n";
+    //     name = orgObj.name;
+    //     age = orgObj.age;
+    //     ptr = orgObj.ptr;
+    // }
+
+    Student(string n, int a, int p){
+        name = n;
+        age = a;
+        ptr = new int;
+        *ptr = p;
+    }
+
+    //__ Display function
+    void display(){
+        cout << endl << name << endl;
+        cout << age << endl;
+        cout << *ptr << endl;
+    }
+
+    // ___ Destructer
+    ~Student(){
+        delete ptr;
+    }
+};
+
+// ___ Main
 int main(){
 
     Hero ironman("Ironman", "Intelligence", "America", 50, 7);
@@ -66,8 +99,35 @@ int main(){
     2. If a Copy Constructor is created by the programmer, it will be
        called instead of Default Copy Constructor. 
 */
-    Hero cap(ironman);
-    cap.display();
+    Hero warMachine=ironman;
+    warMachine.display();
+
+    // Hero cap(ironman);
+    // cap.display();
+
+    Student s1("Mahad", 61, 76);
+    s1.display();
+    
+    Student s2=s1;
+    s1.display();
+    s2.display();
 
     return 0;
 }
+
+/*         SHALLOW COPY (Deafualt)              *              DEEP COPY (Custom)              
+                                                *
+    -> This runs on its own on the backend by   *     -> Customly made when we have to copy pointers
+    the compiler if we have not created any     *        also. Using (new) keyword.
+    any copy constructor.                       *
+                                                *
+    -> Shallow copy can work for many variables *     -> Allows pointers to allocate new memory and
+    but when we are copying pointers, we        *        to copy actual value of the variable of 
+    have to make a custom copy constructor      *        pointers. It's good practice, safe and correct
+    because both pointers copy at the same      *        method while working with pointers.
+    address so many problems are faced.         *
+                                                *
+    -> Hero warMachine=ironman;                 *     -> Student s2=s1;
+    (No Custom Copy Constructor Defined)        *
+                                                *
+*/
