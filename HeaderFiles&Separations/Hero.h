cc34a032b6bcae7c239d2b3538772fedeefe0282
Mahad Abbas <mahadabbas31@gmail.com>
2026-02-25 20:30:10 +0500

    Header File

---
+++ HeaderFiles&Separations/Hero.h
@@ -0,0 +1,48 @@
+#ifndef HERO_H
+#define HERO_H
+
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Hero{
+    string identity;
+    string name;
+    string power;
+    string region;
+    int level;
+    int stars;
+    
+public:
+    Hero();
+    Hero(string i, string n, string p, string r, int l, int s); 
+    Hero(const Hero &obj);
+
+    void display();
+
+        // ___ SetterFunctions
+    void setIdentity(string identity);    
+    void setName(string name);
+    void setPower(string power);
+    void setregion(string region);
+    void setlevel(int level);
+    void setStars(int stars);
+
+    // ___ GetterFunctions
+    string getIdentity();
+    string getName();
+    string getpower();
+    string getregion();
+    int getlevel();
+    int getStars();
+
+    // ___ Level Changing Methods
+    void increaseLevel();
+    void decreaseLevel();
+
+    
+    void championList(Hero arr[], int size);
+};
+
+#endif
