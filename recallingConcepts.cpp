6a2ff135dde278c6ce2520f97dc4f77b86d90eb0
Mahad Abbas <mahadabbas31@gmail.com>
2026-03-14 22:06:30 +0500

    today's recall

--- recallingConcepts.cpp
+++ recallingConcepts.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
 using namespace std;
 
-class Rectangle{
-    int width, height;
-    static int NoOfRectangles;
+// class Rectangle{
+//     int width, height;
+//     static int NoOfRectangles;
+
+// public:
+//     Rectangle(){
+//         width = 0;
+//         height = 0;
+//         NoOfRectangles++;
+//     }
+
+//     static int getTotalRectangles(){
+//         return NoOfRectangles;
+//     }
+        
+// };
+
+// int Rectangle::NoOfRectangles=0;
+
+// int main(){
+//     Rectangle r1;
+//     Rectangle r2;
+//     Rectangle r3;
+//     Rectangle r4;
+//     Rectangle r5;
+//     cout << Rectangle::getTotalRectangles ();
+// }
+
+
+class Test{
+    int i,j;
 
 public:
-    Rectangle(){
-        width = 0;
-        height = 0;
-        NoOfRectangles++;
+    Test(int x, int y){
+        i=x;
+        j=y;
     }
 
-    static int getTotalRectangles(){
-        return NoOfRectangles;
+    void display(){
+        cout << "i: " << i << ", j: " << j << endl;
     }
-        
+
+    void displayArray(Test arr[], int size){
+        for(int k=0; k<size; k++){
+            arr[k].display();
+        }
+    }
 };
 
-int Rectangle::NoOfRectangles=0;
-
 int main(){
-    Rectangle r1;
-    Rectangle r2;
-    Rectangle r3;
-    Rectangle r4;
-    Rectangle r5;
-    cout << Rectangle::getTotalRectangles ();
+
+    Test a(1,1), b(2,2);
+    Test array[2]= {a,b};
+    
+    a.displayArray(array, 2);
 }
