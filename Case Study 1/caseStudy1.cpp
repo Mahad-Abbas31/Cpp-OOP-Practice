d6c20195ab8d0932c5f3d3644bc1f24ba5c59194
Mahad Abbas <mahadabbas31@gmail.com>
2026-03-18 16:42:26 +0500

    Case Study 1

---
+++ Case Study 1/caseStudy1.cpp
@@ -0,0 +1,27 @@
+/*Design a class date through which user must be able to perform
+following operations
+
+  -> Get and set current day, month and year
+  -> Increment by X number of days, months and year
+  -> Set Exam date
+
+Attributes that can be seen in this problem statement are
+
+  -> Day
+  -> Month
+  -> Year
+  -> Exam date (static attribute)
+
+Interfaces
+
+  -> getDay
+  -> getMonth
+  -> getYear
+  -> setDay
+  -> setMonth
+  -> setYear
+  -> addDay
+  -> addMonth
+  -> addYear
+  -> setExamDate
+*/
