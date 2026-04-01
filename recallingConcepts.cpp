887388799cb2d3c84c533d13cfb255d05320a9b6
Mahad Abbas <mahadabbas31@gmail.com>
2026-04-01 23:58:53 +0500

    practicing Operator Overloading for Quiz

--- recallingConcepts.cpp
+++ recallingConcepts.cpp
@@ -1,48 +1,49 @@
-#include <iostream>
+// ___ Operator Overloading
+
+#include<iostream>
 using namespace std;
 
-class Rectangle;  // 👈 Forward declaration
+class Student{
+    string name;
+    int id;
+    int marks[3];
 
-class Square{
-    int	width,	height;  
-  public:
-
-    void set_values(int	x,	int	y);
-    friend void add(Square, Rectangle);
-};
-
-void Square::set_values(int x, int y){  
-    width = x;
-    height = y;
-}
-
-
-class Rectangle{
-    int	width, height;
-  public:
-
-    void set_values(int	x, int y);
-    friend void add(Square, Rectangle);
-
-};
-
-void Rectangle::set_values(int x, int y){  
-    width = x;
-    height = y;
-}
-
-void add(Square A, Rectangle B){
-    cout << "Width = "<< A.width + B.width<< endl;
-    cout << "Height = " << A.height + B.height <<  endl;
-}
-
-int main() {
-    Square s;  
-    Rectangle r ;
-    s.set_values(1,1);
-    r.set_values(2, 3);  
-    add(s, r);
+    public:
     
-    return 0;
-}
+    Student(string name="Unknown", int id=0){
+        this->name = name;
+        this->id = id;
 
+        for(int i=0; i<3; i++)
+            marks[i] = 0;
+    }
+
+    void setName(string name){    this->name = name;    }
+    void set(int id){             this->id = id;        }
+    void set(){
+        for(int i=0; i<3; i++){
+            cout << "\nSet Marks for Subject " << i+1 << ": ";
+            cin >> marks[i];
+        }
+    }
+
+    string getName(){      return name;      }
+    int getId(){           return id;        }
+    int* getMarks() {      return marks;     }
+
+    Student operator+(const Student s){
+        Student temp;
+
+        temp.name = "Combined";
+        temp.id = 0;
+        for(int i=0; i<3; i++)
+            temp.marks[i] = marks[i] + s.marks[i];
+
+        return temp;
+    }
+};
+
+
+int main(){
+    Student s1("Taha", 2501360);
+}
