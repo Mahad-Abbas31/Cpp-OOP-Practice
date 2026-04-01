18bc407fa0b4b8963754745a18c5c58a5ffe6dfc
Mahad Abbas <mahadabbas31@gmail.com>
2026-04-02 00:20:05 +0500

    Practice for Quiz

--- recallingConcepts.cpp
+++ recallingConcepts.cpp
@@ -19,12 +19,14 @@ class Student{
     }
 
     void setName(string name){    this->name = name;    }
-    void set(int id){             this->id = id;        }
-    void set(){
+    void setId(int id){             this->id = id;        }
+    void setMarks(){
         for(int i=0; i<3; i++){
-            cout << "\nSet Marks for Subject " << i+1 << ": ";
+            cout << "Set Marks for Subject " << i+1 
+                 << " for " << name << "(" << id << ")"<< ": ";
             cin >> marks[i];
         }
+        cout << endl;
     }
 
     string getName(){      return name;      }
@@ -41,9 +43,46 @@ class Student{
 
         return temp;
     }
+
+    friend istream &operator>>(istream &input, Student s){
+        cout << "Name: ";
+        input >> s.name;
+        cout << "ID: ";
+        input >> s.id;
+        cout << "*** Marks ***";
+        for(int i=0; i<3; i++){
+            cout << "Subject " << i+1 << ": ";
+            input >> s.marks[i];
+        }
+
+        return input;
+    }
+
+    friend ostream &operator<<(ostream &output, const Student s){
+        output << "Name: " << s.name << endl
+               << "ID : "  << s.id   << endl;
+
+        for(int i=0; i<3; i++)
+            cout << "Subject " << i+1 << ": " << s.marks[i] << endl;
+        
+            return output;
+    }
 };
 
 
 int main(){
     Student s1("Taha", 2501360);
+    s1.setMarks();
+    Student s2("Aisha", 2501363);
+    s2.setMarks();
+    Student s3("Tooba", 2501369);
+    s3.setMarks();
+    Student s4("Zaryab", 2501348);
+    s4.setMarks();
+    
+    Student allStudents;
+
+    allStudents = s1 + s2 + s3 + s4;
+
+    cout << allStudents;
 }
